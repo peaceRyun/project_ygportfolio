95cbad44cfe93c4f00dc4e0980a1142e4966b6d5
peaceRyun <dbsrn1110@gmail.com>
2025-05-03 21:00:04 +0900

    framermotion 라이브러리 설치 // intro => 퍼블리싱 구현1

---
+++ src/app/home/components/banner/PerformanceB.jsx
@@ -0,0 +1,5 @@
+const PerformanceB = () => {
+  return <div>performanceB</div>;
+};
+
+export default PerformanceB;
--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,5 +1,17 @@
+import PerformanceB from "./components/banner/PerformanceB";
+import Intro from "./components/intro/Intro";
+import ProjectList from "./components/project/ProjectList";
+
 const HomePage = () => {
-    return <div className='text-primary w-full'>sss</div>;
+  const mainCont = "w-full relative max-w-[1920px] bg-white mx-auto";
+
+  return (
+    <main className={`${mainCont}`}>
+      <Intro />
+      <PerformanceB />
+      <ProjectList />
+    </main>
+  );
 };
 
 export default HomePage;
