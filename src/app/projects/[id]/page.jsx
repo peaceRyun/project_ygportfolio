6cac23bac413f98a4c9ef494d3ac7c628dc3b1af
peaceRyun <dbsrn1110@gmail.com>
2025-05-05 15:02:59 +0900

    project=> 제목 레이아웃 일부 수정. //data => 개복치 process 내용 일부 수정.

--- src/app/projects/[id]/page.jsx
+++ src/app/projects/[id]/page.jsx
@@ -37,7 +37,7 @@ const ProjectPage = () => {
         <>
             <section className='relative w-full h-[587px] overflow-hidden max-lg:h-[510px]'>
                 <h2 className='sr-only'>{title}</h2>
-                <div className='absolute top-1/2 left-0 -translate-y-1/2 mx-auto flex flex-col justify-center z-20 w-full max-w-[1400px] px-8 max-lg:max-w-auto max-sm:max-w-auto max-sm:px-5'>
+                <div className='absolute top-1/2 left-1/2 -translate-1/2 flex flex-col justify-center z-20 w-full mx-auto max-w-[1400px] px-12 max-lg:max-w-auto max-lg:px-8 max-sm:max-w-auto max-sm:px-5'>
                     <h3 className='text-white text-6xl font-bold'>{title}</h3>
                 </div>
                 <Image src={src} alt={alt} className='absolute top-0 left-0 z-0 blur-sm' fill />
@@ -45,9 +45,9 @@ const ProjectPage = () => {
                 <div className='waveAni absolute bottom-[-76px] left-0 w-[5000px] h-[247px] z-30 object-contain max-lg:bottom-0 max-lg:h-[160px]'></div>
             </section>
             <main className='w-full max-w-[1400px] mx-auto relative z-10 pt-[7.5rem] px-12 max-lg:px-8 max-sm:px-5'>
-                <div className='contents-wrap flex justify-between items-stretch max-lg:flex-col max-lg:gap-20'>
+                <div className='contents-wrap flex gap-24 justify-between items-stretch max-lg:flex-col max-lg:gap-20'>
                     <div className='info-area flex-1'>
-                        <div className='sticky top-[160px] left-0 w-full pr-12 max-lg:pr-8 max-sm:pr-5 max-lg:relative max-lg:top-0'>
+                        <div className='sticky top-[160px] left-0 w-full max-lg:relative max-lg:top-0'>
                             <div className='flex flex-col items-start w-full gap-10'>
                                 <h4 className='text-4xl font-semibold whitespace-pre-wrap'>{titledetail}</h4>
                                 <div className='flex flex-col gap-4'>
@@ -66,7 +66,7 @@ const ProjectPage = () => {
                         </div>
                     </div>
                     <div className='detail-area flex-1'>
-                        <div className='h-[2000px] flex flex-col gap-4 pl-12 max-lg:pl-8 max-sm:pl-5'>
+                        <div className='h-[2000px] flex flex-col gap-4'>
                             <section>
                                 <h5 className='font-bold text-2xl'>Period</h5>
                                 <p>
@@ -103,7 +103,7 @@ const ProjectPage = () => {
                                             <li key={item.title} className='inline mr-4'>
                                                 <p className='mb-3 text-xl'>{item.title}</p>
                                                 <div className='flex flex-col gap-1'>
-                                                    <p>{item.desc}</p>
+                                                    <p className='whitespace-pre-wrap'>{item.desc}</p>
                                                 </div>
                                             </li>
                                         ))}
