e7c7b1c1e2de481659019fc7d746f7c1afbe1b88
peaceRyun <dbsrn1110@gmail.com>
2025-05-07 16:25:35 +0900

    data 내용 수정 + 아이콘 이미지 소스 추가.

--- src/app/home/components/banner/PerformanceB.jsx
+++ src/app/home/components/banner/PerformanceB.jsx
@@ -56,7 +56,7 @@ const PerformanceB = () => {
                         ref={textObjectRef}
                     >
                         "당신이 능력을 최대한 발휘해서 실행하는 데 주의 를 기울이고 있다면, <br /> 당신은
-                        <span className='highlight bg-clip-text bg-no-repeat bg-gradient-to-t from-[#d49a65] to-[#d49a65] bg-[size:100%_0%] text-transparent'>
+                        <span className='highlight bg-clip-text bg-no-repeat bg-gradient-to-t from-[#d49a65] to-[#d49a65] bg-[size:100%_0%] text-transparent px-1'>
                             성과 영역
                         </span>
                         에 있는 것이다."
