2098a8642213097d85ae310b6cf5740f453f9272
peaceRyun <dbsrn1110@gmail.com>
2025-05-15 16:14:00 +0900

    fix => eslint수정. build error 관련.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,39 +1,42 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginPrettier from 'eslint-plugin-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals"),
-  {
-    plugins: ["prettier"],
-    rules: {
-      "prettier/prettier": "warn", // 또는 "error"
-      "arrow-body-style": "off",
-      curly: "off",
-      "lines-around-comment": "off",
-      "max-len": "off",
-      "no-confusing-arrow": "off",
-      "no-mixed-operators": "off",
-      "no-tabs": "off",
-      "no-unexpected-multiline": "off",
-      "object-curly-newline": "off",
-      "operator-linebreak": "off",
-      quotes: "off",
-      semi: "off",
-      "space-before-blocks": "off",
-      "space-before-function-paren": "off",
-      "space-in-parens": "off",
-      "space-infix-ops": "off",
-      "template-curly-spacing": "off",
+    ...compat.extends('next/core-web-vitals'),
+    {
+        plugins: {
+            prettier: pluginPrettier,
+        },
+        rules: {
+            'prettier/prettier': 'warn',
+            'arrow-body-style': 'off',
+            curly: 'off',
+            'lines-around-comment': 'off',
+            'max-len': 'off',
+            'no-confusing-arrow': 'off',
+            'no-mixed-operators': 'off',
+            'no-tabs': 'off',
+            'no-unexpected-multiline': 'off',
+            'object-curly-newline': 'off',
+            'operator-linebreak': 'off',
+            quotes: 'off',
+            semi: 'off',
+            'space-before-blocks': 'off',
+            'space-before-function-paren': 'off',
+            'space-in-parens': 'off',
+            'space-infix-ops': 'off',
+            'template-curly-spacing': 'off',
+        },
     },
-  },
 ];
 
 export default eslintConfig;
