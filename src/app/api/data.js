b46497c07e5379f0ab2ec60d9e89771e67ba0128
peaceRyun <dbsrn1110@gmail.com>
2025-05-19 10:35:08 +0900

    데이터 순번 최신순으로 수정.

--- src/app/api/data.js
+++ src/app/api/data.js
@@ -1,5 +1,157 @@
 export const portfoliodata = [
     {
+        id: 4,
+        title: 'VanCleef',
+        titledetail: '쥬얼리 브랜드&쇼핑몰 웹페이지\n리뉴얼',
+        src: '/home/displayportfolio/thumb/vancleef.png',
+        alt: '팀포폴250226',
+        thumbstartdate: '2025.02',
+        period_start: '2025.02.26',
+        period_end: '2025.03.24',
+        desc: '기존 반클리프 아펠 사이트를 기능까지 리메이크 해본 React Vite 웹 개발 협업 프로젝트 입니다. 불편한 UX와 동적 인터렉티브 요소의 부족을 개선해 보았습니다. 로컬스토리지를 활용해보기 위해 목업 데이터 구조 설계부터 시작하여 장바구니, 상품별 리뷰&문의, 로그인&회원가입, 회원정보 불러오기&수정 등을 구현 하였습니다. ',
+        type: [
+            { name: '팀', classname: 'bg-gray-300 text-gray-700' },
+            { name: '레퍼런스 리메이크', classname: 'bg-yellow-300 text-yellow-700' },
+            { name: '와이어 프레임', classname: 'bg-teal-300 text-teal-700' },
+            { name: '요구사항 명세서', classname: 'bg-emerald-300 text-emerald-700' },
+            { name: 'UI 디자인', classname: 'bg-sky-300 text-sky-700' },
+            { name: '목업 데이터 구조 설계', classname: 'bg-rose-300 text-rose-700' },
+        ],
+        projecttype: ['사이드'],
+        deploy: 'https://project-vancleef.vercel.app',
+        code: 'https://github.com/peaceRyun/project-vancleef',
+        figma: 'https://www.figma.com/design/mmwiSjjdtvvVi1dB5dsiZt/D2C-Project?node-id=1-88&t=IsmeicsJpaB2Dogg-1',
+        techStack: ['React', 'Next.js', 'Tailwind CSS', 'Sass', 'Javascript', 'Gsap'],
+        requests: [
+            '반클리프&아펠 사이트를 조사한 결과, 투박한 디자인과 복잡한 메뉴 구조가 최신 웹 트렌드를 적용하여 개선된다면 2030 세대의 사이트 이용률을 높일 수 있을 것이라고 판단했습니다. 보다 상세한 요구사항명세서도 작성해보면서 기능설계에 대한 계획도 짜볼 수 있었습니다.',
+        ],
+        process: [
+            {
+                title: '리서치 및 기획',
+                desc: '이미지 소스가 많은 웹사이트를 레퍼런스로 채택해보며 직접 JSON 객체형태의 객체 데이터를 활용하여 다양한 아이템들에 대한 정보를 나열하며 기능 중심의 구현을 해보기로 기획하게 되었습니다.',
+            },
+            {
+                title: '와이어프레임',
+                desc: 'Figma를 사용하여 와이어프레임을 제작하여 사용자 중심의 레이아웃을 제작했습니다. 설계 과정에서 어떤 레퍼런스를 참고할지에 대한 결정, 레이아웃 크기 설계, 사용자 흐름에 따라 어떤 페이지를 넣어볼지와 같은 상의를 figma를 통해 실시간으로 협업해볼 수 있었습니다.',
+            },
+            {
+                title: 'UI 디자인',
+                desc: '방문자가 귀금속에 집중할 수 있도록 무채색 계열로 깔끔하게 디자인했습니다. 시각적 만족도를 높이기 위해 고품질 png와 svg 이미지와 아이콘을 활용하고, 명품브랜드를 깔끔하고 세련된 타이포그래피 느낌을 주기 위해 세리프체를 사용하며 프로토타입 디자인을 완성하였습니다.',
+            },
+            {
+                title: '구현',
+                desc: '상품 페이지, 장바구니, 결제/예약, 리뷰/문의와 같은 필수 이커머스 기능을 사용자 친화적으로 웹사이트를 개발했습니다. 특히 마이페이지에서 모든 기록을 관리할 수 있는 페이지로 만들기 위해 로컬스토리지의 데이터 관리에 신경을 많이 쓰게 되었습니다. ',
+            },
+        ],
+        keyFeatures: [
+            {
+                title: '사용자 인증 시스템',
+                video: '/projects/vancleef/vancleef-login.mp4',
+                desc1: '로컬스토리지 기반 로그인/회원가입 기능 구현, 세션 유지 및 마이페이지 기록 연동 구현',
+            },
+            {
+                title: '제품 카테고리 필터링',
+                video: '/projects/vancleef/vancleef-search.mp4',
+                desc1: '리스트페이지에서는 직관적인 4개의 카테고리로 구성하고, new, best, 가격순으로도 제품들을 볼 수 있습니다.',
+                desc2: '검색 기능은 제품명 검색 외에도, 컬렉션명이나 시리즈명으로도 검색이 가능하도록 구현하였습니다.',
+            },
+            {
+                title: '화려하면서 깔끔한 디자인 페이지',
+                video: '/projects/vancleef/vancleef-gsap.mp4',
+                desc1: '로딩, 랜딩페이지, 전시회페이지, About페이지에서 Gsap을 활용한 깔끔한 스크롤 인터렉션 페이지를 만들어 사용자가 브랜드에 대해 보다 직관적이고 재밌게 접근하게 만들었습니다.',
+            },
+        ],
+        troubleShooting: [
+            {
+                title: '목업 데이터 객체 구조 최적화',
+                problem:
+                    '리뉴얼 과정에서 제품 상세정보와 원하는 이미지를 효율적인 코드로 가져오기 위해 구조 설계가 필요하였습니다. 어떤 데이터를 노출시킬지에 대한 고민을 하며 이를 쉽게 가공하기 위해 호출하기 쉬운 적합한 구조가 무엇인지 고민을 하였습니다.',
+                solution:
+                    '학습에 이용되었던 목업데이터 코드를 참고하며 배열객체 형식의 큰 뼈대를 짜고, 여러 값이 들어가는 것에 대해서 redux에서 initialstate와 어떤 dispatch함수 만들지에 대한 고려를 하며 최대한 단순하면서도 효과적으로 객체를 구성할 수 있도록 하였습니다.',
+            },
+        ],
+    },
+    {
+        id: 5,
+        title: 'VIBO',
+        titledetail: 'OTT 자체기획 홈페이지',
+        src: '/home/displayportfolio/thumb/vibo.png',
+        alt: '팀포폴241231',
+        thumbstartdate: '2024.12',
+        period_start: '2024.12.31',
+        period_end: '2025.02.25',
+        desc: '상업성 높은 3개의 사이트(넷플릭스, 왓챠, 티빙)을 재해석하여 탄생한 OTT 사이트 “ViBo”입니다.  React Vite를 활용해 콘텐츠별 리뷰 기능 및 UI 개선, 동적 애니메이션을 통한 홍보 강화를 통해 기존 사이트들의 부족한 사용자 경험을 보완하여 만든 순수 React 사이트 입니다. 4명의 협업을 통해 만들어졌습니다.',
+        type: [
+            { name: '팀', classname: 'bg-gray-300 text-gray-700' },
+            { name: 'TMDB API', classname: 'bg-slate-300 text-slate-700' },
+            { name: '와이어 프레임', classname: 'bg-teal-300 text-teal-700' },
+            { name: '요구사항 명세서', classname: 'bg-emerald-300 text-emerald-700' },
+            { name: 'UI 디자인', classname: 'bg-sky-300 text-sky-700' },
+            { name: 'flowChart', classname: 'bg-stone-300 text-stone-700' },
+        ],
+        projecttype: ['사이드'],
+        deploy: 'https://viboott.vercel.app/',
+        code: 'https://github.com/peaceRyun/vibo',
+        figma: 'https://www.figma.com/design/2RnUhIuvGnwXrh5v91NX04/OTT-작업지침서?node-id=0-1&t=Vk9L49iYm3Ad85gQ-1',
+        techStack: ['React', 'Next.js', 'Tailwind CSS', 'Sass', 'Javascript', 'Gsap'],
+        requests: [
+            ' OTT의 핵심기능은 그대로 구현하면서, 상세페이지에 실시간 리뷰 정보를 얻을 수 있는 섹션을 만들어 정보력에 민감한 2030 OTT 사용자를 겨냥했습니다. 시각적인 효과에도 몰입감을 느낄 것이라고 생각하였기에, 군데군데 동적 인터렉티브 요소들을 배치하여 시선을 끌어보았습니다.',
+        ],
+        process: [
+            {
+                title: '리서치 및 기획',
+                desc: '타깃 고객과 경쟁사를 이해하기 위해 시장 조사를 진행했습니다. 마인드맵 방식을 통해 유즈케이스 분석을 하여 사용자가 어떤 경험을 하게 될 것인지에 대한 flow를 생각해보았습니다. primary color를 선택하고, Rough 디자인을 페이지 단위로 분담하여 진행하였습니다.',
+            },
+            {
+                title: '와이어프레임 및 프로토타이핑',
+                desc: 'Rough 디자인을 토대로Figma를 사용하여 와이어프레임과 프로토타입을 제작하여 보다 세밀한 사용자 중심의 레이아웃을 제작했습니다.',
+            },
+            {
+                title: '시각 디자인',
+                desc: '방문자가 ViBo에 집중할 수 있도록 검정색 배경에 Primary컬러 계열로 깔끔하게 디자인했습니다. 시각적 만족도를 높이기 위해 고품질 이미지와 깔끔한 타이포그래피를 사용했습니다.',
+            },
+            {
+                title: '구현',
+                desc: '컨텐츠 페이지, 마이페이지 기록 관리, 결제 구독 관리와 같은 필수 OTT 기능을 탑재하여 웹사이트를 개발했습니다.',
+            },
+        ],
+        keyFeatures: [
+            {
+                title: 'TMDB API 활용 콘텐츠 컨셉별 Swiper 및 검색 기능',
+                video: '/projects/vibo/vibo-search.mp4',
+                desc1: 'TMDB API를 활용하여 요일별 신작 Swiper, 인기 Top10 Swiper, 장르별 리스트, 검색, 애니메이션 Swiper, 다크한 분위기 영화 Swiper, 감성 드라마 Swiper 를 구현하였습니다. ',
+            },
+            {
+                title: '로컬 스토리지 기반 로그인/회원가입, 프로필 아이콘, 마이페이지 기록 관리 기능 구현',
+                video: '/projects/vibo/vibo-login.mp4',
+                desc1: 'Local Storage를 활용하여 로그인/회원가입 기능을 구현했습니다. 간단한 비밀번호 유효성 검사와 중복 닉네임 검사까지 구현하여 보안 강화와 가입 사용자에 대한 구별하였습니다.',
+                desc2: '개인화된 기록관리를 할 수 있도록, 관심 콘텐츠, 시청 기록, 리뷰/평점 기록을 동적으로 그리고 한눈에 볼 수 있도록 구현하였습니다.',
+                desc3: '프로필 아이콘 변경 페이지를 통해 디자이너가 직접 디자인한 아이콘을 선택하고 저장되도록 구현하였습니다.',
+            },
+            {
+                title: '곳곳에 있는 사용자 인터렉션 요소',
+                video: '/projects/vibo/vibo-interaction.mp4',
+                desc1: '사용자가 사이트에 흥미를 느끼고, 군데군데 홍보 요소에 대해 거부감을 느끼지 않도록 애니메이션을 추가하였습니다.',
+            },
+        ],
+        troubleShooting: [
+            {
+                title: 'Swiper: Overflow 문제',
+                problem:
+                    '인기 Top 10 Swiper를 개발하면서 원래 기획 의도대로 구현하는 것이 생각보다 어렵다는 것을 알게 되었습니다. 본래 기술 스택에서 Styled-Coponent를 골랐음에도 Swiper를 제어하기 위해서는 인라인 style을 써야하거나 별도의 css의 제어가 필요했습니다. 그러다보니 style 우선순위 문제로 swiper의 내용물이 튀어나오거나, 튀어나와야 하는 요소가 나오지 못하고 레이아웃에 갇히는 등 의도한 레이아웃이 나오지 않는 문제가 발생했습니다.',
+                solution:
+                    '이를 해결하기 위해 챗봇, 구글링 검색, 레퍼런스 사이트 코드 분석을 통해 Swiper(부모)와 swiper-slide(자식)을 overflow:visible를 주고, 이 Swiper의 부모요소 RightArea에 padding-left와 overflow:hidden을 줌으로써 원하는 부분만 나오게 하여 피그마 원래 디자인이 나오게끔 구현하였습니다.',
+            },
+            {
+                title: '메인페이지 버벅임 문제',
+                problem:
+                    '최종 메인페이지 결과물에는 hover시에 나오는 목업데이터 썸네일 영상, GSAP 애니메이션 효과, API로 끌어와지는 데이터들로 인해 같은 순간에 로드가 되면서 충돌이 일어나고, 렉이 걸리면서 사이트를 스크롤 할 수 없을 만큼의 버벅임이 발생하게 되었습니다. ',
+                solution:
+                    '이를 순서를 정해놓고 지연을 발생시키는 방법을 고안하고 구현하니 해결이 되었습니다. 우선순위는 가장 먼저 떠야하는 GSAP 애니메이션을 1순위로 두고, 그다음 컨텐츠인 라이브 목업 hover 영상, 마지막으로 API로 끌어와지는 데이터를 3초, 5초로 1차적으로 지연을 두었습니다. 이렇게 하니 자연스럽게 과부화가 오는 현상을 해결할 수 있었고, 로드가 안되는 부분은 로딩 애니메이션과 페이지를 추가하여 사용자가 알 수 있게끔 처리하였습니다.  ',
+            },
+        ],
+    },
+    {
         id: 1,
         title: 'PRETCOORD',
         titledetail: '패션 3D 플랫폼 사이트\n랜딩 페이지 코드 리메이크',
@@ -190,159 +342,6 @@ export const portfoliodata = [
                     'GSAP을 처음 사용해보면서 to와 from을 통한 애니메이션 조정 방식을 이해를 하면서 조금씩 구현을 시도 해보았습니다. 하지만 생각만큼의 부드러운 동작이 이루어지지 않았습니다. 또한, 코드의 간결성 또한 떨어져 같은 코드의 반복이 지나치게 많이 생기면서 변수관리도 복잡해지고 가독성도 떨어져 코드 수정이 점점 어려워졌습니다.',
                 solution:
                     '레퍼런스 분석을 통해 css만 제어하되, GSAP으로 클래스를 동적으로 붙이는 방법을 알아내어 보다 더 부드럽고, 적은 양의 코딩으로 같은 효과를 나타낼 수 있었습니다. ScrollTigger의 create 속성 중 start값에 퍼센티지로 부여하는 방법을 알아내면서, 다양한 화면사이즈에 따라 동일한 타이밍에 애니메이션 효과를 구현해볼 수 있었습니다. 또한, 코드가 간소화 되면서 여러 사용자들로 부터 타이밍과 CSS 조절을 하게될때도 관련 지표(속성)가 직관적으로 보여서 효율적으로 수정해볼 수 있었습니다.',
-            },
-        ],
-    },
-
-    {
-        id: 4,
-        title: 'VanCleef',
-        titledetail: '쥬얼리 브랜드&쇼핑몰 웹페이지\n리뉴얼',
-        src: '/home/displayportfolio/thumb/vancleef.png',
-        alt: '팀포폴250226',
-        thumbstartdate: '2025.02',
-        period_start: '2025.02.26',
-        period_end: '2025.03.24',
-        desc: '기존 반클리프 아펠 사이트를 기능까지 리메이크 해본 React Vite 웹 개발 협업 프로젝트 입니다. 불편한 UX와 동적 인터렉티브 요소의 부족을 개선해 보았습니다. 로컬스토리지를 활용해보기 위해 목업 데이터 구조 설계부터 시작하여 장바구니, 상품별 리뷰&문의, 로그인&회원가입, 회원정보 불러오기&수정 등을 구현 하였습니다. ',
-        type: [
-            { name: '팀', classname: 'bg-gray-300 text-gray-700' },
-            { name: '레퍼런스 리메이크', classname: 'bg-yellow-300 text-yellow-700' },
-            { name: '와이어 프레임', classname: 'bg-teal-300 text-teal-700' },
-            { name: '요구사항 명세서', classname: 'bg-emerald-300 text-emerald-700' },
-            { name: 'UI 디자인', classname: 'bg-sky-300 text-sky-700' },
-            { name: '목업 데이터 구조 설계', classname: 'bg-rose-300 text-rose-700' },
-        ],
-        projecttype: ['사이드'],
-        deploy: 'https://project-vancleef.vercel.app',
-        code: 'https://github.com/peaceRyun/project-vancleef',
-        figma: 'https://www.figma.com/design/mmwiSjjdtvvVi1dB5dsiZt/D2C-Project?node-id=1-88&t=IsmeicsJpaB2Dogg-1',
-        techStack: ['React', 'Next.js', 'Tailwind CSS', 'Sass', 'Javascript', 'Gsap'],
-        requests: [
-            '반클리프&아펠 사이트를 조사한 결과, 투박한 디자인과 복잡한 메뉴 구조가 최신 웹 트렌드를 적용하여 개선된다면 2030 세대의 사이트 이용률을 높일 수 있을 것이라고 판단했습니다. 보다 상세한 요구사항명세서도 작성해보면서 기능설계에 대한 계획도 짜볼 수 있었습니다.',
-        ],
-        process: [
-            {
-                title: '리서치 및 기획',
-                desc: '이미지 소스가 많은 웹사이트를 레퍼런스로 채택해보며 직접 JSON 객체형태의 객체 데이터를 활용하여 다양한 아이템들에 대한 정보를 나열하며 기능 중심의 구현을 해보기로 기획하게 되었습니다.',
-            },
-            {
-                title: '와이어프레임',
-                desc: 'Figma를 사용하여 와이어프레임을 제작하여 사용자 중심의 레이아웃을 제작했습니다. 설계 과정에서 어떤 레퍼런스를 참고할지에 대한 결정, 레이아웃 크기 설계, 사용자 흐름에 따라 어떤 페이지를 넣어볼지와 같은 상의를 figma를 통해 실시간으로 협업해볼 수 있었습니다.',
-            },
-            {
-                title: 'UI 디자인',
-                desc: '방문자가 귀금속에 집중할 수 있도록 무채색 계열로 깔끔하게 디자인했습니다. 시각적 만족도를 높이기 위해 고품질 png와 svg 이미지와 아이콘을 활용하고, 명품브랜드를 깔끔하고 세련된 타이포그래피 느낌을 주기 위해 세리프체를 사용하며 프로토타입 디자인을 완성하였습니다.',
-            },
-            {
-                title: '구현',
-                desc: '상품 페이지, 장바구니, 결제/예약, 리뷰/문의와 같은 필수 이커머스 기능을 사용자 친화적으로 웹사이트를 개발했습니다. 특히 마이페이지에서 모든 기록을 관리할 수 있는 페이지로 만들기 위해 로컬스토리지의 데이터 관리에 신경을 많이 쓰게 되었습니다. ',
-            },
-        ],
-        keyFeatures: [
-            {
-                title: '사용자 인증 시스템',
-                video: '/projects/vancleef/vancleef-login.mp4',
-                desc1: '로컬스토리지 기반 로그인/회원가입 기능 구현, 세션 유지 및 마이페이지 기록 연동 구현',
-            },
-            {
-                title: '제품 카테고리 필터링',
-                video: '/projects/vancleef/vancleef-search.mp4',
-                desc1: '리스트페이지에서는 직관적인 4개의 카테고리로 구성하고, new, best, 가격순으로도 제품들을 볼 수 있습니다.',
-                desc2: '검색 기능은 제품명 검색 외에도, 컬렉션명이나 시리즈명으로도 검색이 가능하도록 구현하였습니다.',
-            },
-            {
-                title: '화려하면서 깔끔한 디자인 페이지',
-                video: '/projects/vancleef/vancleef-gsap.mp4',
-                desc1: '로딩, 랜딩페이지, 전시회페이지, About페이지에서 Gsap을 활용한 깔끔한 스크롤 인터렉션 페이지를 만들어 사용자가 브랜드에 대해 보다 직관적이고 재밌게 접근하게 만들었습니다.',
-            },
-        ],
-        troubleShooting: [
-            {
-                title: '목업 데이터 객체 구조 최적화',
-                problem:
-                    '리뉴얼 과정에서 제품 상세정보와 원하는 이미지를 효율적인 코드로 가져오기 위해 구조 설계가 필요하였습니다. 어떤 데이터를 노출시킬지에 대한 고민을 하며 이를 쉽게 가공하기 위해 호출하기 쉬운 적합한 구조가 무엇인지 고민을 하였습니다.',
-                solution:
-                    '학습에 이용되었던 목업데이터 코드를 참고하며 배열객체 형식의 큰 뼈대를 짜고, 여러 값이 들어가는 것에 대해서 redux에서 initialstate와 어떤 dispatch함수 만들지에 대한 고려를 하며 최대한 단순하면서도 효과적으로 객체를 구성할 수 있도록 하였습니다.',
-            },
-        ],
-    },
-    {
-        id: 5,
-        title: 'VIBO',
-        titledetail: 'OTT 자체기획 홈페이지',
-        src: '/home/displayportfolio/thumb/vibo.png',
-        alt: '팀포폴241231',
-        thumbstartdate: '2024.12',
-        period_start: '2024.12.31',
-        period_end: '2025.02.25',
-        desc: '상업성 높은 3개의 사이트(넷플릭스, 왓챠, 티빙)을 재해석하여 탄생한 OTT 사이트 “ViBo”입니다.  React Vite를 활용해 콘텐츠별 리뷰 기능 및 UI 개선, 동적 애니메이션을 통한 홍보 강화를 통해 기존 사이트들의 부족한 사용자 경험을 보완하여 만든 순수 React 사이트 입니다. 4명의 협업을 통해 만들어졌습니다.',
-        type: [
-            { name: '팀', classname: 'bg-gray-300 text-gray-700' },
-            { name: 'TMDB API', classname: 'bg-slate-300 text-slate-700' },
-            { name: '와이어 프레임', classname: 'bg-teal-300 text-teal-700' },
-            { name: '요구사항 명세서', classname: 'bg-emerald-300 text-emerald-700' },
-            { name: 'UI 디자인', classname: 'bg-sky-300 text-sky-700' },
-            { name: 'flowChart', classname: 'bg-stone-300 text-stone-700' },
-        ],
-        projecttype: ['사이드'],
-        deploy: 'https://viboott.vercel.app/',
-        code: 'https://github.com/peaceRyun/vibo',
-        figma: 'https://www.figma.com/design/2RnUhIuvGnwXrh5v91NX04/OTT-작업지침서?node-id=0-1&t=Vk9L49iYm3Ad85gQ-1',
-        techStack: ['React', 'Next.js', 'Tailwind CSS', 'Sass', 'Javascript', 'Gsap'],
-        requests: [
-            ' OTT의 핵심기능은 그대로 구현하면서, 상세페이지에 실시간 리뷰 정보를 얻을 수 있는 섹션을 만들어 정보력에 민감한 2030 OTT 사용자를 겨냥했습니다. 시각적인 효과에도 몰입감을 느낄 것이라고 생각하였기에, 군데군데 동적 인터렉티브 요소들을 배치하여 시선을 끌어보았습니다.',
-        ],
-        process: [
-            {
-                title: '리서치 및 기획',
-                desc: '타깃 고객과 경쟁사를 이해하기 위해 시장 조사를 진행했습니다. 마인드맵 방식을 통해 유즈케이스 분석을 하여 사용자가 어떤 경험을 하게 될 것인지에 대한 flow를 생각해보았습니다. primary color를 선택하고, Rough 디자인을 페이지 단위로 분담하여 진행하였습니다.',
-            },
-            {
-                title: '와이어프레임 및 프로토타이핑',
-                desc: 'Rough 디자인을 토대로Figma를 사용하여 와이어프레임과 프로토타입을 제작하여 보다 세밀한 사용자 중심의 레이아웃을 제작했습니다.',
-            },
-            {
-                title: '시각 디자인',
-                desc: '방문자가 ViBo에 집중할 수 있도록 검정색 배경에 Primary컬러 계열로 깔끔하게 디자인했습니다. 시각적 만족도를 높이기 위해 고품질 이미지와 깔끔한 타이포그래피를 사용했습니다.',
-            },
-            {
-                title: '구현',
-                desc: '컨텐츠 페이지, 마이페이지 기록 관리, 결제 구독 관리와 같은 필수 OTT 기능을 탑재하여 웹사이트를 개발했습니다.',
-            },
-        ],
-        keyFeatures: [
-            {
-                title: 'TMDB API 활용 콘텐츠 컨셉별 Swiper 및 검색 기능',
-                video: '/projects/vibo/vibo-search.mp4',
-                desc1: 'TMDB API를 활용하여 요일별 신작 Swiper, 인기 Top10 Swiper, 장르별 리스트, 검색, 애니메이션 Swiper, 다크한 분위기 영화 Swiper, 감성 드라마 Swiper 를 구현하였습니다. ',
-            },
-            {
-                title: '로컬 스토리지 기반 로그인/회원가입, 프로필 아이콘, 마이페이지 기록 관리 기능 구현',
-                video: '/projects/vibo/vibo-login.mp4',
-                desc1: 'Local Storage를 활용하여 로그인/회원가입 기능을 구현했습니다. 간단한 비밀번호 유효성 검사와 중복 닉네임 검사까지 구현하여 보안 강화와 가입 사용자에 대한 구별하였습니다.',
-                desc2: '개인화된 기록관리를 할 수 있도록, 관심 콘텐츠, 시청 기록, 리뷰/평점 기록을 동적으로 그리고 한눈에 볼 수 있도록 구현하였습니다.',
-                desc3: '프로필 아이콘 변경 페이지를 통해 디자이너가 직접 디자인한 아이콘을 선택하고 저장되도록 구현하였습니다.',
-            },
-            {
-                title: '곳곳에 있는 사용자 인터렉션 요소',
-                video: '/projects/vibo/vibo-interaction.mp4',
-                desc1: '사용자가 사이트에 흥미를 느끼고, 군데군데 홍보 요소에 대해 거부감을 느끼지 않도록 애니메이션을 추가하였습니다.',
-            },
-        ],
-        troubleShooting: [
-            {
-                title: 'Swiper: Overflow 문제',
-                problem:
-                    '인기 Top 10 Swiper를 개발하면서 원래 기획 의도대로 구현하는 것이 생각보다 어렵다는 것을 알게 되었습니다. 본래 기술 스택에서 Styled-Coponent를 골랐음에도 Swiper를 제어하기 위해서는 인라인 style을 써야하거나 별도의 css의 제어가 필요했습니다. 그러다보니 style 우선순위 문제로 swiper의 내용물이 튀어나오거나, 튀어나와야 하는 요소가 나오지 못하고 레이아웃에 갇히는 등 의도한 레이아웃이 나오지 않는 문제가 발생했습니다.',
-                solution:
-                    '이를 해결하기 위해 챗봇, 구글링 검색, 레퍼런스 사이트 코드 분석을 통해 Swiper(부모)와 swiper-slide(자식)을 overflow:visible를 주고, 이 Swiper의 부모요소 RightArea에 padding-left와 overflow:hidden을 줌으로써 원하는 부분만 나오게 하여 피그마 원래 디자인이 나오게끔 구현하였습니다.',
-            },
-            {
-                title: '메인페이지 버벅임 문제',
-                problem:
-                    '최종 메인페이지 결과물에는 hover시에 나오는 목업데이터 썸네일 영상, GSAP 애니메이션 효과, API로 끌어와지는 데이터들로 인해 같은 순간에 로드가 되면서 충돌이 일어나고, 렉이 걸리면서 사이트를 스크롤 할 수 없을 만큼의 버벅임이 발생하게 되었습니다. ',
-                solution:
-                    '이를 순서를 정해놓고 지연을 발생시키는 방법을 고안하고 구현하니 해결이 되었습니다. 우선순위는 가장 먼저 떠야하는 GSAP 애니메이션을 1순위로 두고, 그다음 컨텐츠인 라이브 목업 hover 영상, 마지막으로 API로 끌어와지는 데이터를 3초, 5초로 1차적으로 지연을 두었습니다. 이렇게 하니 자연스럽게 과부화가 오는 현상을 해결할 수 있었고, 로드가 안되는 부분은 로딩 애니메이션과 페이지를 추가하여 사용자가 알 수 있게끔 처리하였습니다.  ',
             },
         ],
     },
